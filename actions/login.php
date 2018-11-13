096ec9ddd4c0ff34302815eadaa6842364618339
Victor <vb4007@gmail.com>
2018-11-13 18:00:39 +0000

    fixup! Login ok

--- actions/login.php
+++ actions/login.php
@@ -11,6 +11,7 @@ $result = $soapClient->login($params)->return;
 if ($result == "-1" || $result == "0") {
   header('Location: ../login.php?err=-1');
 } else {
+  session_start();
   $_SESSION['user_id'] = $result;
   header('Location: ../');
 }
