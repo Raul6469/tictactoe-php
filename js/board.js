0e8a623ad03c4b46b79126f8ad9e08638892af91
Victor <vb4007@gmail.com>
2018-11-14 15:28:48 +0000

    Update board display each second

--- js/board.js
+++ js/board.js
@@ -1,10 +1,19 @@
-$("document").ready(() => {
-  var url = new URL(window.location.href);
-  var gid = url.searchParams.get("gid");
+let url;
+let gid;
 
+$("document").ready(() => {
+  url = new URL(window.location.href);
+  gid = url.searchParams.get("gid");
+
+  updateBoardDisplay();
+})
+
+function updateBoardDisplay() {
   $.get("ajax/board.php?gid=" + gid , function( data ) {
     data.forEach(move => {
       $("td[x="+move.x+"][y="+move.y+"]").text("X");
     });
   });
-})
+
+  setTimeout(updateBoardDisplay, 1000);
+}
