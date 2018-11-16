8a9756632c43d6da33752e7918ae1db67f3f77f1
Victor <vb4007@gmail.com>
2018-11-16 13:32:21 +0000

    Get user stats services

--- common/services.php
+++ common/services.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("entities/game.php");
+require_once("entities/stats.php");
 
 class Services {
   public static function showOpenGames() {
@@ -39,6 +40,52 @@ class Services {
     $result = $soapClient->joinGame($params)->return;
 
     return $result;
+  }
+
+  public static function getUserStats($uid, $username) {
+    require_once("common/soap-client.php");
+
+    $params = array(
+      "uid" => $uid
+    );
+
+    $gamesString = $soapClient->showAllMyGames($params)->return;
+
+    $gameStrings = explode("\n", $gamesString);
+
+    $stats = new Stats();
+
+    foreach($gameStrings as $gameString) {
+      $game = new Game();
+      $gameElems = explode(",", $gameString);
+
+      $game->id = $gameElems[0];
+      $game->p1 = $gameElems[1];
+      $game->p2 = $gameElems[2];
+      
+      $checkWinParams = array(
+        "gid" => $game->id,
+      );
+
+      try {
+        $gameState = $soapClient->checkWin($checkWinParams)->return;
+        $game->state = $gameState;
+      } catch(Exception $e) {
+        continue;
+      }
+
+      if ($game->state === "0") {
+        // Game still in progress
+      } else if ($game->state == "3") {
+        $stats->draws++;
+      } else if (($game->state == "1" && $game->p1 == $username) || ($game->state == "2" && $game->p2 == $username)) {
+        $stats->wins++;
+      } else {
+        $stats->losses++;
+      }
+    }
+
+    return $stats;
   }
 
 }
---
+++ entities/stats.php
@@ -0,0 +1,12 @@
+<?php
+class Stats {
+  public $wins;
+  public $losses;
+  public $draws;
+
+  public function __construct() {
+    $this->wins = 0;
+    $this->losses = 0;
+    $this->draws = 0; 
+  }
+}
